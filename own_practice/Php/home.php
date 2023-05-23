45d2c16975c9316df280aeb5301db7ccb6c8343a
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2023-05-23 20:52:29 +0530

    updated

--- own_practice/Php/home.php
+++ own_practice/Php/home.php
@@ -8,7 +8,52 @@
 </head>
 <body>
     <?php
-   echo "<h1>My first PHP script!</h1>";
+   echo "<h1>Hello World!</h1>";
+    $age=18;
+    if ($age>=18){
+        echo"You'r eligible for vote.<br>";
+
+    }else{
+        echo"You'r not eligible for vote.<br>";
+    }
+    //switch statement
+
+    $op='/';
+    switch($op) {
+        case '+':
+                echo"Choose Addition";
+                break;
+        case '-':
+                echo"Choose Substraction";
+                break;
+        default:
+                echo "Wrong" ;      
+    }
+
+    //loops
+    //for loop
+    echo"<h2>Using For loop.</h2>";
+    for($i=0;$i<=10;$i++){
+        echo "The numbers are $i <br>";
+    }
+
+    //while loop
+    echo"<h2>Using While loop.</h2>";
+    $i=1;
+    while ($i<=10){
+        echo "The numbers are $i <br>";
+        $i++;
+    }
+
+    //do whileloop
+    echo"<h2>Using do while loop.</h2>";
+    $i=1;
+    do{
+       echo "The numbers are $i <br>";
+        $i++;  
+    }while($i<=10)
+
+
    ?>
 </body>
 </html>
