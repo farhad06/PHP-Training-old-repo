5743290677229f91fecd68f10c6ec1325a8d4686
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2023-06-13 01:30:29 +0530

    kk

--- own_practice/Php/class-1.php
+++ own_practice/Php/class-1.php
@@ -2,7 +2,7 @@
 //default time zone is paris/france 
 // we change the default time zone by using date_default_timezone_set() function. 
 date_default_timezone_set('Asia/Kolkata');
-$date=date('d-m-y h-i-A');
+$date=date('d-M-Y h-i-A');
 echo "Current time is: ".$date;
 
 
