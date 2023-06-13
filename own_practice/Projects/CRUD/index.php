3cdb951fd5925b50f42c875b87c91cea3bb6ba07
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2023-06-13 12:48:03 +0530

    change+added

---
+++ own_practice/Projects/CRUD/index.php
@@ -0,0 +1,58 @@
+<?php include ('header.php'); ?>
+        <div id="main-contain" style="margin-top:3px;">
+        <h2>All Records</h2>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Class</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>
+                              <button class="btn btn-sm btn-info"><a href='#'>Edit</a></button>
+                              <button class="btn btn-sm btn-danger"><a href='#'>Delete</a></button>
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@mdo</td>
+                        <td>
+                            <button class="btn btn-sm btn-info"><a href='#'>Edit</a></button>
+                            <button class="btn btn-sm btn-danger"><a href='#'>Delete</a></button>
+                        </td>
+                        <!-- <td>@mdo</td> -->
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry the Bird</td>
+                        <td>Tweeter</td>
+                        <td>@twitter</td>
+                        <td>@mdo</td>
+                        <td>
+                           <button class="btn btn-sm btn-info"><a href='#'>Edit</a></button>
+                           <button class="btn btn-sm btn-danger"><a href='#'>Delete</a></button>
+                        </td>
+                        <!-- <td>@mdo</td> -->
+                    </tr>
+                    
+                </tbody>
+                </table>
+        </div>
+</div>
+</body>
+</html>
